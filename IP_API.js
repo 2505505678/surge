7eb5833defd10fc4f75a92743c9899bc53acb502
TrollStore2 <115724601+TrollStore2@users.noreply.github.com>
2024-04-10 02:31:22 +0800

    Update IP_API.js

--- IP_API.js
+++ IP_API.js
@@ -247,7 +247,7 @@ var flags = new Map([
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = flags.get(obj["countryCode"]) + " " + City_ValidCheck(obj["city"]); //+Area_check(obj['country']);
-var subtitle = obj['as'];
+var subtitle = obj['query'];
 var ip = obj["query"];
 var description =
   "服务商:" +
